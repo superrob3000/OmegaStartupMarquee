5f38cba1b52a5333681689e5c9723d67e99d4a5b
superrob3000 <robbracero@gmail.com>
2021-12-30 02:30:57 -0500

    Add project files.

---
+++ OmegaStartupMarquee/OmegaStartupMarquee_SystemEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace OmegaStartupMarquee
+{
+    public class OmegaStartupMarquee_SystemEventHandler : ISystemEventsPlugin
+    {
+        void ISystemEventsPlugin.OnEventRaised(string eventType)
+        {
+            if (eventType == SystemEventTypes.BigBoxStartupCompleted)
+            {
+                //Kill the marquee startup video now that the main screen video has completed.
+                Process[] Process = System.Diagnostics.Process.GetProcesses();
+                for (int i = 0; i < Process.Length; i++)
+                {
+                    if (Process[i].ProcessName.StartsWith("BigBoxWithStartupMarquee"))
+                    {
+                        Process[i].Kill();
+                    }
+                }
+            }
+        }
+    }
+}
